24d636e35d6fae6814f36989674a83c08ea266d5
BugHunter-X <askzenai@gmail.com>
2024-12-26 17:36:08 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,5 @@
+This error occurs when using the Expo CLI to manage an Expo project and involves issues with the `eas build` command, specifically when building for Android. The error message might not be very descriptive, sometimes simply saying that the build failed without clear reasons. This can be due to various factors, such as incorrect configuration in the `android` directory, problems with the build environment (Gradle, Android SDK), or even temporary issues with the Expo servers.  Here's an example of a vague error message you might encounter:
+
+`Error: Build failed`
+
+Debugging this requires systematically checking multiple aspects of the project setup and build process.
